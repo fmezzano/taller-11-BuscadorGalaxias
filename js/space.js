6a4db324717bf37b8b9fd959580eb56976055e80
vainntn <valentin.acosta512@gmail.com>
2023-09-06 20:46:06 -0300

    updateGalaxias

--- js/space.js
+++ js/space.js
@@ -1,42 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const busqueda = document.getElementById('inputBuscar')
-    const boton = document.getElementById('btnBuscar')
-    const container = document.getElementById('contenedor')
+  const busqueda = document.getElementById('inputBuscar')
+  const boton = document.getElementById('btnBuscar')
+  const container = document.getElementById('contenedor')
 
-    boton.addEventListener('click', function () {
-        const buscado = busqueda.value;
-        container.innerHTML = ""
-        fetch(`https://images-api.nasa.gov/search?q=${buscado}`)
-            .then((response) => response.json())
-            .then((data) => {
-                const items = data.collection.items;
-                items.forEach((items) => {
-                    const title = items.data[0].title;
-                    const description = items.data[0].description;
-                    const date = items.data[0].date_created;
-                    const img = items.links[0].href;
-
-                    const aMostrar = `
-                <div class="list-group-item list-group-item-action cursor-pointer">
-                <div class="row">
-                  <div class="col-3">
-                    <img src="${img}" alt="${title}" class="img-thumbnail">
-                  </div>
-                  <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                      <h4 class="mb-1">${title}</h4>
-                      <small class="text-muted">Fecha: ${date}</small>
-                    </div>
-                    <p class="mb-1">${description}</p>
-                  </div>
+  boton.addEventListener('click', function () {
+    const buscado = busqueda.value;
+    container.innerHTML = ""
+    fetch(`https://images-api.nasa.gov/search?q=${buscado}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const items = data.collection.items;
+        items.forEach((items) => {
+          const title = items.data[0].title;
+          const description = items.data[0].description;
+          const date = items.data[0].date_created;
+          const img = items.links[0].href;
+          const aMostrar = `
+          <div class="col-md-4 mt-4">
+            <div class="card h-100">
+              <img class="card-img-top" src="${img}" alt="Card image cap" style="height: 200px; object-fit: cover;">
+              <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+                <div class="card-text" style="max-height: 100px; overflow-y: auto;">
+                  ${description}
                 </div>
               </div>
-                `
-                    container.innerHTML += aMostrar
-                })
-            })
-            .catch((error) => {
-                console.error('Error al realizar la solicitud a la API:', error);
-            })
-    })
+              <h6 class="card-subtitle mb-2 text-muted">${date}</h6>
+            </div>
+          </div>
+`
+          container.innerHTML += aMostrar
+        })
+      })
+      .catch((error) => {
+        console.error('Error al realizar la solicitud a la API:', error);
+      })
+  })
 })
